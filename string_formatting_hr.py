0638a3fc409b253ebd626c26f94e0f1dbff37ea9
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-14 23:47:06 +0530

    Add files via upload

---
+++ string_formatting_hr.py
@@ -0,0 +1,13 @@
+"""
+string formating using python(HackerRank)
+"""
+
+# def print_formatted(number):
+#     # your code goes here
+#     l = bin(number)[2:].__len__()
+#     for i in range(1, number+1, 1):
+#         octal = hexadecimal = binary = int(i)
+#         print("{0:{1}d} {0:{1}o} {0:{1}X} {0:{1}b}".format(i, l))
+# if __name__ == '__main__':
+#     n = int(input())
+#     print_formatted(n)
